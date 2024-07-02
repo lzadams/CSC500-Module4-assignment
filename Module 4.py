601a8a814c426cab4c72a8b840cc9e607608ace1
lzadams <lili.adams@csuglobal.edu>
2024-07-01 23:25:51 -0700

    Update Module 4.py

--- Module 4.py
+++ Module 4.py
@@ -9,25 +9,22 @@ class ItemToPurchase:
         return total_cost
 
 #Step 2: In the main section of your code, prompt the user for two items and create two objects of the ItemToPurchase class.
-
-print('Item 1')
-item1 = ItemToPurchase()
-item1.name = input('Enter the item name:\n')
-item1.price = float(input('Enter the item price:\n'))
-item1.quantity = int(input('Enter the item quantity:\n'))
-item1_cost = item1.print_item_cost()
-
-print('Item 2')
-item2 = ItemToPurchase()
-item2.name = input('Enter the item name:\n')
-item2.price = float(input('Enter the item price:\n'))
-item2.quantity = int(input('Enter the item quantity:\n'))
-item2_cost = item2.print_item_cost()
+items = [] 
+for i in range(2):
+    print(f'Item {i + 1}')
+    item = ItemToPurchase()
+    item.name = input('Enter the item name:\n')
+    item.price = float(input('Enter the item price:\n'))
+    item.quantity = int(input('Enter the item quantity:\n'))
+    items.append(item)
+    item_cost = item.print_item_cost()
+    i = i + 1
 
 #Step 3: Add the costs of the two items together and output the total cost.
 print('TOTAL COST')
-print(f'{item1.name} {item1.quantity} @ ${item1.price} = ${item1_cost}')
-print(f'{item2.name} {item2.quantity} @ ${item2.price} = ${item2_cost}')
-total_cost = item1_cost + item2_cost
-print(f'Total: ${total_cost}')
+total_cost = 0 
+for item in items:
+    total_cost += item.print_item_cost()
+    print(f'Total: ${total_cost}')
+
 
